dbe1748430779421c35d42df7844346f6c64cced
Christianto Budisaputra <53547157+chrsomle@users.noreply.github.com>
2021-09-02 12:13:07 +0700

    Add Travis CI

---
+++ GameeTests/GameeTests.swift
@@ -0,0 +1,20 @@
+//
+//  GameeTests.swift
+//  GameeTests
+//
+//  Created by Christianto Budisaputra on 02/09/21.
+//
+
+import XCTest
+
+class GameeTests: XCTestCase {
+
+    func testTrue() throws {
+        XCTAssertTrue(true)
+    }
+
+    func testFalse() throws {
+        XCTAssertTrue(false)
+    }
+    
+}
